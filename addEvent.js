92443b6563319b241797610dd9a2ab08e9cd72e5
Alex Bilyi <beluy452@yandex.ru>
2017-09-07 15:53:06 +0300

    refactore code

--- addEvent.js
+++ addEvent.js
@@ -1,6 +1,7 @@
 function addEvent(selector,event,callback) {
     var items = document.querySelectorAll(selector);
-    for(var i = 0; i < items.length; i++){
-        items[i].addEventListener(event,callback);
+    var itemLen = items.length;
+    while(itemLen--) {
+        items[itemLen].addEventListener(event,callback);
     }
 }
